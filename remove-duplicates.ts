84bfdf0cdc1fbeb26a86cac079f34493c88b25ec
thomas/NERO <tomconmo@gmail.com>
2018-04-22 16:25:10 -0400

    first homework!

---
+++ remove-duplicates.ts
@@ -0,0 +1,27 @@
+/*
+   Remove Duplicates
+
+   Hi! Welcome to your first homework assignment. :) You'll be writing a function called removeDuplicates that takes a single parameter, a *sorted* array of numbers
+   and returns an array of numbers with all duplicates removed. That is to say, if you receive an array of [1, 2, 3, 3, 4, 5]... the function will return [1, 2, 3, 4, 5]
+
+   After that, (or before, if you want) you will write a function called removeDuplicateCharacters that removes duplicate characters in a string.
+   (e.g. "aardvaark" returns "ardvark")
+   Note that it's probably best to approach this with stringbuilding because strings are *immutable*
+
+   You can test your code by opening your Terminal, navigating to the 'sandbox' directory, and then typing `jest` and hitting ENTER (heretoafter known as 'running jest')
+   Jest is a testing framework that we will play with more in the future, but for now, don't worry about it too much. Jest will run two tests, one for each function
+   and let you know if they pass or fail.
+
+   Don't forget to run `npm install` first! (also inside the 'sandbox' directory), and remember to Google things when you get stuck!
+*/
+
+export function removeDuplicates (rng: number[])
+{
+   // your code here
+}
+
+
+export function removeDuplicateCharacters (str: string)
+{
+   // your code here
+}
